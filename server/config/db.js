896833df3de95526408834fb3f9deaaa1f4bbc7c
toboo <toboos@sina.com>
2016-05-06 03:19:58 +0800

    edit mongo service config

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
+
+var port = process.env.MONGODB_PORT_27017_TCP_PORT;
+var addr = process.env.MONGODB_PORT_27017_TCP_ADDR;
+var instance = process.env.MONGODB_INSTANCE_NAME;
+var password = process.env.MONGODB_PASSWORD;
+var username = process.env.MONGODB_USERNAME;
+// 'mongodb://user:pass@localhost:port/database'
+mongoose.connect('mongodb://' + username + ':' + password +'@' + addr + ':' + port + '/' + instance);
+
 // mongodb 数据库
-mongoose.connect('localhost:27017/test_full');
+//mongoose.connect('localhost:27017/test_full');
 
 var db = mongoose.connection; // 返回当前mongodb数据库连接
 db.on('error', function(err){
